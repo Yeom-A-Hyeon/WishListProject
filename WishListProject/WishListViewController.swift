07186447ba28fc76660dd9cfb1407c17177566d4
Yeom-A-Hyeon <20180982@dongduk.ac.kr>
2024-04-18 17:32:05 +0900

    create WishListViewController storyboard and swift file

---
+++ WishListProject/WishListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WishListViewController.swift
+//  WishListProject
+//
+//  Created by A Hyeon on 4/18/24.
+//
+
+import UIKit
+
+class WishListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
